d1b23904be4e0d840b728413299f4030ba652bb3
CharlieBMF <marcinski.karol@gmail.com>
2023-02-09 10:06:37 +0100

    initial

---
+++ VS.py
@@ -0,0 +1,35 @@
+import pymcprotocol
+
+
+class VisionSystem:
+
+    def __int__(self, id_line, id_machine, name, ip, port, target_network=None, plc_id_in_target_network=None,):
+        self.id_line = id_line
+        self.id_machine = id_machine
+        self.name = name
+        self.ip = ip
+        self.port = port
+        self.target_network = target_network
+        self.plc_id_in_target_network = plc_id_in_target_network
+
+    def define_machine_root(self):
+        pymc3e = pymcprotocol.Type3E()
+        if self.target_network and self.plc_id_in_target_network:
+            pymc3e.network = self.target_network
+            pymc3e.pc = self.plc_id_in_target_network
+        return pymc3e
+
+    def connect(self):
+        self.machine.connect(ip=self.ip, port=self.port)
+
+    def close_connection(self):
+        self.machine.close()
+
+    def read_bits(self, head, size=1):
+        return self.machine.batchread_bitunits(headdevice=head, readsize=size)
+
+    def read_words(self, head, size=1):
+        return self.machine.batchread_wordunits(headdevice=head, readsize=size)
+
+    def read_random_words(self, word_devices, double_word_devices):
+        return self.machine.randomread(word_devices=word_devices, dword_devices=double_word_devices)
---
+++ conf.py
@@ -0,0 +1,18 @@
+machines_names = {
+    'Gas_Generant':
+        {
+            'id_line': 33,
+            'id_machine': 240,
+            'name': 'GG',
+            'ip': '192.168.10.161',
+            'port': 40021,
+            'address':
+                {
+                    'master_on_address': 'M6',
+                    'machine_status_address': 'M44',
+                    'mct_address': 'D50080',
+                },
+            'target_network': None,
+            'plc_id_in_target_network': None
+        },
+}
